602a395aa03822da56d4644ceae600e4e5f3d51c
kaushik chaudhary <kaushikbchaudhary@gmail.com>
2021-12-30 18:59:49 +0530

    JM-2 lecture1

---
+++ core-java/src/lecture1/Test.java
@@ -0,0 +1,73 @@
+package lecture1;
+
+public class Test {
+    public static void main(String[] args) {
+ /*
+         Identifiers
+         name in java programing used for identifiers.
+         name in java program which can be concerned as identifiers.
+         name in java program is called identifiers, which can be used for identification purpose.
+         it can be method name or variable name, class name or label name.
+        class text {
+            public static void main(string args){
+                int x=10;
+                }
+         }
+         Name in java program is by default concerned as 'Identifiers'.
+
+         I.rules for defining java identifiers
+         1.the only allowed characters in java identifiers are
+             a to z //small
+             A to Z // capital
+             0 to 9 // Digit
+             $ ,_  // dollar sign and underscore
+             if you're using any other characters we will get compile time error
+             total_number   -> valid
+             total#  ->invalid
+         2.identifiers can't start's with digit
+         total123 ->valid
+         123total -> in valid
+         3.java identifiers are case-sensitive
+            java language case-sensitive
+            of course java language itself is tried as case-sensitive programming language
+            in java identifiers how many characters are allowed
+            class text{
+                int number = 10;    |
+                int Number = 20;    |we can differentiate with 2 case
+                int NUMBER = 30;    |
+                }
+        4.there is no length of limits for java identifiers,
+        but it is not recommended to lengthy identifiers.
+        5.we can't use reserve word as identifiers
+           int x = 10;
+           int if = 20;
+           Next terminology
+        6.all predefined java class name and interface name we can happily use as identifiers
+            class Text{
+                public static void main (strings[] args){
+                    int strings = 888;
+                    system.out.println(stings);
+                    int runnable = 999;
+                    system.out.println(runnable);
+                    }
+                }
+            even those it's valid,
+            but it's not good programming practice because it reduces readability and create confusion.
+
+            Q-which of the following are valid java identifiers
+            total_number //valid
+            total#  //invalid
+            123total  //invalid
+            total123 //valid
+            ca$h //valid
+            _$_$_$_$_$_  //valid
+            all@hands //invalid
+            java2Share //valid
+            Integer //valid
+            Int // valid
+            int // invalid
+
+
+         */
+    }
+}
---
+++ core-java/src/lecture1/Test2.java
@@ -0,0 +1,95 @@
+package lecture1;
+
+public class Test2 {
+    public static void main(String[] args) {
+       /*
+       Reserved word (53)
+       In java some words are reserved word to represent some meaning or functionality
+       such type of word are called reserved word.
+       total reserved word ->53
+       Reserved Literals(3)
+       keyword(50)     |->       used keyword(48)  | if-else.....       unused keyword(2) | goto , const
+       keywords for data types
+       byte short int long float double boolean char
+       keywords for flow control
+       if else switch case default while do for break continue return // 11
+
+       keywords for modifiers
+       *public privet protected static find abstract synchronised native strictfp(1.2v) arnsient(1.2v) volatile // 11
+
+       keywords for exception handling
+       try catch finally throw, throws assert(1.4w) // 6
+
+       class related keyword
+       class interface extends implements package import // 6
+
+       object related keyword
+       new instanceof super this // 4
+
+       void return type keyword
+       void // 1
+
+       in java return type is mandatory if you method return anything than we how to declare that method with
+       void return type
+       but in c language return type is optional and default return type is int.
+
+       unused keyword(2)
+       1.goto | used of goto create a several problem in go languages enhance some people band this keyword in java
+       2.const | use of final instead of const
+       goto and const are unused keyword and if you are trying to use we will get compile time error
+
+       Reserved literals(3)
+       true & false |values for boolean data type
+       null | default value for object reference
+
+       enum keyword(1.5)
+       we can use enum to define a group of named constant
+       enum month{
+            jan,feb,...,dec;
+            }
+       enum bear{
+            KF,KO,RC,FO;
+            }
+
+       conclusion
+       All 53 reserved word in java contains only lowercase alphabet symbol
+       InJAVA we have only new keyword and there is no delete keyword because destruction of use less object is the
+       responsibility of garbage collector
+
+       the following are new keyword in java
+       strictfp came in 1.2 version
+       assert came in 1.4 version
+       enum came in 1.5 version
+
+       strictfp but not strictfp
+       instanceof but not instanceof
+       synchronised but not synchronised
+       extend but not extend
+       implements but not implements
+       import but not implements
+       const but not const but not implements
+
+       which of the following list contain only java reserved word
+       new,delete
+       goto,constant
+       break,continue,return,exit
+       final,finally,finalize
+       throw,throws,thrown,
+       notify,notify All
+       implements,extends,imports
+       sizeof,instanceof,strictfp
+       byte,short,int
+       None of the above
+
+       which of the following are java reserved words
+       public   >
+       static   >
+       void     >
+       main
+       string   >its predefined classname
+       args
+       first 3
+
+        */
+    }
+}
